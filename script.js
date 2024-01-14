7e4cc31f7d5d6e0134309e99e98fa4d98a62bffb
tata.zhigalova <tata.zhigalova.99@bk.ru>
2024-01-15 00:34:34 +0300

    Реализовала список покупок с помощью JS

--- script.js
+++ script.js
@@ -7,3 +7,26 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+
+const enterInput = document.querySelector('#input');
+const buyItems = document.querySelector('.items');
+
+
+enterInput.addEventListener('keydown', function(event) {
+    if (event.key == 'Enter') {
+        const itemText = enterInput.value;
+        const newList = document.createElement('div');
+        newList.classList.add('list');
+        newList.textContent = itemText;
+
+    newList.addEventListener ('click', function () {
+        newList.classList.toggle('done');
+       });
+
+    if (itemText != '') {
+        buyItems.append(newList);
+    }
+    enterInput.value = '';
+    }
+});
